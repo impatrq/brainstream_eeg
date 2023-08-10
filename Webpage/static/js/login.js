8a5e988b4357c40fd5e224a551fcfd26fd46f6d6
Fede <80333258+federicoprzybylski@users.noreply.github.com>
2023-08-10 08:54:50 -0300

    Add files via upload

---
+++ Webpage/static/js/login.js
@@ -0,0 +1,8 @@
+let intro = document.querySelector(".intro")
+let logo = document.querySelector(".logo")
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() =>{
+        intro.style.display = 'none'
+    }, 5100)
+});
