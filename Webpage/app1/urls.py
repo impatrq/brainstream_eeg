88548ebe7d5fe3ec2bce347490b075d7f83abc84
Fede <80333258+federicoprzybylski@users.noreply.github.com>
2023-08-10 08:56:20 -0300

    Add files via upload

---
+++ Webpage/app1/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('inicio', views.inicio, name='inicio'),
+    path('resultn', views.result, name='resultado'),
+    path('bcin', views.bciin, name='bcii'),
+    path('contacto', views.contactoin, name='contacto'),
+    path('acercade', views.acercain, name='acercade'),
+    path('cognitive', views.cognitive, name='cognitive'),
+    path('historial', views.historial, name='historial'),
+
+]
