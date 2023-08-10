2a31fbc61de2abb1d8fe0ff6b34c9c327e5573e4
Fede <80333258+federicoprzybylski@users.noreply.github.com>
2023-08-10 08:52:03 -0300

    Delete Webpage/brainstream directory

--- Webpage/brainstream/app1/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .consumers import GraphConsumer
-
-ws_urlpatterns = [
-    path("menu/resultn", GraphConsumer.as_asgi()),
-]
--- Webpage/brainstream/static/js/login.js
+++
@@ -1,8 +0,0 @@
-let intro = document.querySelector(".intro")
-let logo = document.querySelector(".logo")
-
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() =>{
-        intro.style.display = 'none'
-    }, 5100)
-});
