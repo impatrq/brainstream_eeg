ed8923c43bfaa503e257bf53bb5dd87014b5a4eb
Thomas Kaufmann <thomibernal@gmail.com>
2023-11-16 00:22:07 -0300

    Feat agregar campos_visualización(admin.py)

--- Webpage/brainstream/app1/admin.py
+++ Webpage/brainstream/app1/admin.py
@@ -10,7 +10,11 @@ class RequestDemoAdmin(admin.ModelAdmin):
 
 
 class DatosAdmin(admin.ModelAdmin):
-    list_display = ('id', 'valores')  # Campos que deseas mostrar en la lista
+    list_display = ('id',
+                     'valores0', "valores1", 'valores2', "valores3",
+                     'valores4', "valores5", 'valores6', "valores7",
+                     'valores8', "valores9", 'valores10', "valores11",
+                     'valores12', "valores13", 'valores14', "valores15",)  # Campos que deseas mostrar en la lista
 
 # Registra el modelo y la clase personalizada en el administrador
 admin.site.register(Datos, DatosAdmin)
