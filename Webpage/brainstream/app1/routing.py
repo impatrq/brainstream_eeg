642db0355a188ec77720192a0c05fbb0f440fbe4
Thomas Kaufmann <thomibernal@gmail.com>
2023-11-16 00:26:21 -0300

    Fix arreglar routing.py

--- Webpage/brainstream/app1/routing.py
+++ Webpage/brainstream/app1/routing.py
@@ -1,7 +1,16 @@
+from channels.routing import ProtocolTypeRouter,URLRouter
 from django.urls import path
-
+from channels.auth import AuthMiddlewareStack
+import os
+from django.core.asgi import get_asgi_application
 from .consumers import GraphConsumer
 
-ws_urlpatterns = [
-    path("menu/resultn", GraphConsumer.as_asgi()),
-]
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path("menu/resultn", GraphConsumer.as_asgi()),
+        ])
+    )
+})
