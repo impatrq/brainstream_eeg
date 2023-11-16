5c823b9dc3b718545002936b49c0fdac0101e2bf
Thomas Kaufmann <thomibernal@gmail.com>
2023-11-16 00:28:17 -0300

    Feat agregar creacion_carpetas

--- Webpage/brainstream/app1/views.py
+++ Webpage/brainstream/app1/views.py
@@ -13,6 +13,13 @@ encoding = sys.getdefaultencoding()
 
 def inicio(request):
     if request.user.is_authenticated:
+        username = request.user.username
+        ruta_carpeta_imagenes_prev = os.path.join("static", "img", "historial", username, "preview")
+        # ruta_carpeta_np = os.path.join("static", "img", "historial", username, "informes")
+        ruta_carpeta_edf= os.path.join("static", "img", "historial", username, "download")
+        ruta_carpeta_np= os.path.join("static", "img", "historial", username, "array")
+        for directory_path in [ruta_carpeta_imagenes_prev, ruta_carpeta_np, ruta_carpeta_edf]:
+            os.makedirs(directory_path, exist_ok=True)
         return render(request, "content/inicio.html")
     else:
         return redirect(reverse("welcome"))
